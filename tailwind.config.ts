87561f5770101addc6286efc8cb4f2f0c5e7427b
faizm10 <faizmustansar10@gmail.com>
2024-03-26 02:14:30 -0400

    homepage

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+const plugin = require('tailwindcss/plugin');
 
 const config: Config = {
   content: [
@@ -7,11 +8,23 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        'primary': '#0E1E5B', 
+        'secondary': '#091442',
+        'tertiary': '#3562A6', 
+        'fourth' : '#6594C0',
+        'fifth' : '#0B0B0B'
+      },
+      fontFamily: {
+        sans: ['Graphik', 'sans-serif'],
+        serif: ['Merriweather', 'serif'],
       },
     },
   },
