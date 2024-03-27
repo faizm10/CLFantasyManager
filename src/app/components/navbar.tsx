c1df49675eb7861488e5127f28ebeb22a954b507
faizm10 <faizmustansar10@gmail.com>
2024-03-27 11:27:06 -0400

    function stubs + navbar

---
+++ src/app/components/navbar.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Link,
+} from "@nextui-org/react";
+
+export default function NavBar() {
+  return (
+    <Navbar
+      style={{
+        backgroundColor: "#3573cf",
+        position: "sticky",
+        top: 0,
+        zIndex: 1000,
+        height: "60px", // Increased height of navbar
+        padding: "0 20px", // Added padding for larger appearance
+      }}
+      isBordered
+    >
+      <NavbarBrand>
+      </NavbarBrand>
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        <NavbarItem>
+          <Link style={{ color: "white", fontSize: "16px" }} href="/"> {/* Increased font size for links */}
+            Home
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link style={{ color: "white", fontSize: "16px" }} href="/playerStats"> {/* Increased font size for links */}
+            Player Stats
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify="end"></NavbarContent>
+    </Navbar>
+  );
+}
