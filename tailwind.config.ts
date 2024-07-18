d62bf5e35f0c7afedde3efd1d0a7a04a41d52ef7
faizm10 <faizmustansar10@gmail.com>
2024-07-18 17:47:11 -0400

    playerstats

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import {nextui} from '@nextui-org/theme';
 import type { Config } from "tailwindcss";
 const plugin = require('tailwindcss/plugin');
 
@@ -6,6 +7,7 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/components/[object Object].js"
   ],
   theme: {
     
@@ -28,6 +30,7 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
 };
 export default config;
